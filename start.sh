3e8840a7605da6efeeec73967489da7343191732
Nathan-Yinka <oludarenathaniel@ggmail.com>
2025-03-01 21:35:30 +0100

    edied

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ alembic upgrade head
 
 # Start the application
 echo "Starting the application..."
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:$PORT
+gunicorn -w 1 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:$PORT
